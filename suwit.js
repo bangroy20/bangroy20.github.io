8e504693796fa4ab4b65c0735adcb006b79c2acc
Achmad Syaifudin <bangroy8420@gmail.com>
2022-02-13 21:06:05 +0700

    upload game suwit

---
+++ suwit.js
@@ -0,0 +1,47 @@
+var nama = prompt("Masukan nama kamu :");
+var tanya = true;
+while (tanya) {
+  var p = prompt("Tuliskan Pilihanmu : \nbatu, gunting, kertas !!");
+
+  var comp = Math.random();
+
+  if (comp < 0.34) {
+    comp = "batu";
+  } else if (comp >= 0.34 && comp < 0.67) {
+    comp = "gunting";
+  } else {
+    comp = "kertas";
+  }
+
+  var hasil = "";
+
+  if (p == comp) {
+    hasil = "SERI !!";
+  } else if (p == "batu") {
+    if (comp == "gunting") {
+      hasil = "MENANG";
+    } else {
+      hasil = "KALAH";
+    }
+    //   hasil = comp == "gunting" ? "MENANG" : "KALAH";
+  } else if (p == "gunting") {
+    if (comp == "kertas") {
+      hasil = "MENANG";
+    } else {
+      hasil = "KALAH";
+    }
+    //   hasil = comp == "batu" ? "KALAH" : "MENANG";
+  } else if (p == "kertas") {
+    if (comp == "batu") {
+      hasil = "MENANG";
+    } else {
+      hasil = "KALAH";
+    }
+    //   hasil = comp == "gunting" ? "KALAH" : "MENANG";
+  } else {
+    hasil = "memasukan pilihan yang salah !!";
+  }
+  alert(nama + ", Kamu memilih " + p + " Dan Syaifudin memilih " + comp + "\nkamu " + hasil);
+
+  tanya = confirm("Main Lagi??");
+}
